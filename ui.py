28df470fe4c074f05897f1051e741c22932872a6
necrolust@gmail.com <dkc9655u6962>
2019-12-28 20:16:34 +1300

    more features

---
+++ ui.py
@@ -0,0 +1,22 @@
+# example_form.py
+from adminui import *
+
+app = AdminApp()
+
+def on_submit(form_data):
+    print(form_data)
+
+@app.page('/', 'form')
+def form_page():
+    return [
+        Form(on_submit = on_submit, content = [
+            TextField('Title', required=True),
+            TextArea('Description'),
+            FormActions(content = [
+                SubmitButton('Submit')
+            ])
+        ])
+    ]
+
+if __name__ == '__main__':
+    app.run()
